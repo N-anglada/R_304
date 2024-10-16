f63279703c3f59ff37ac107f1b6ba7031e0f31e7
nicolas <nicolas.anglada@etu.unilim.fr>
2024-10-16 22:54:15 +0200

    j'avais pas enregistré

--- pizza/src/main/java/pizza/State.java
+++ pizza/src/main/java/pizza/State.java
@@ -1,5 +1,12 @@
 package pizza;
 
-public class State {
-
+public abstract class State {
+	protected Pizza pizza;
+	
+	public State(Pizza pizza) {
+		this.pizza=pizza;
+	}
+	
+	public abstract boolean bake();
+	public abstract boolean deliver();
 }
